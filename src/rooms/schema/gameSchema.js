ffc124a106e11ff588c75ddfd28a89f013bd4128
lurch9 <90292267+lurch9@users.noreply.github.com>
2022-01-11 18:02:11 -0800

    replacing MySchema
    Added player and room schema

---
+++ src/rooms/schema/gameSchema.js
@@ -0,0 +1,48 @@
+const schema = require('@colyseus/schema');
+const { Room } = require('colyseus');
+
+class MyRoomState extends schema.Schema {
+  constructor(){
+    super();
+    this.mySynchronizedProperty = "Hello world";
+  }
+}
+
+schema.defineTypes(MyRoomState, {
+  mySynchronizedProperty: "string",
+});
+
+class Player extends Schema {
+  constructor(teamNumber) {
+    super();
+    this.name = "";
+    this.color = "";
+    this.x = 0;
+    this.y = 0;
+    this.teamNumber = teamNumber;
+  }
+}
+defineTypes(Player, {
+  name: "string",
+  color: "number",
+  x: "number",
+  y: "number",
+  teamNumber: "number",
+});
+
+class RoomSchema extends Schema {
+  constructor() {
+    super();
+    this.teams = new ArraySchema();
+    this.players = new MapSchema();
+  }
+ }
+ defineTypes(RoomSchema, {
+  teams: [Team], // an Array of Team
+  players: { map: Player } // a Map of Players
+ });
+
+exports.RoomSchema = RoomSchema;
+exports.Player = Player;
+exports.MyRoomState = MyRoomState;
+
